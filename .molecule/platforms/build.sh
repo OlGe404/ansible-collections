43aebabf516767e0c344051e3fc38c8662f97cd4
OlGe404 <surelyNotPublic@gmail.com>
2024-06-08 11:45:10 +0200

    update galaxy.yml to include more files

--- .molecule/platforms/build.sh
+++ .molecule/platforms/build.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
+#!/bin/bash -xe
 
 DOCKERFILE=$1
 TIMESTAMP=$(date +%Y%m%d%H%M%S)
 IMAGE_TAG="$(basename $DOCKERFILE | cut -d . -f2-)-$TIMESTAMP"
+IMAGE_REPO=${2:-docker.io/olge404/molecule}
 
 podman build --file $DOCKERFILE --tag docker.io/olge404/molecule:$IMAGE_TAG .
-podman push docker.io/olge404/molecule:$IMAGE_TAG
+podman push $IMAGE_REPO:$IMAGE_TAG
