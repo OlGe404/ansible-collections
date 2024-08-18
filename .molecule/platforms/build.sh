c6c608573b6e2a7bbb66e651bb2c17aaed839c77
OlGe404 <surelyNotPublic@gmail.com>
2024-08-18 12:00:33 +0200

    quick save

--- .molecule/platforms/build.sh
+++ .molecule/platforms/build.sh
@@ -5,5 +5,5 @@ TIMESTAMP=$(date +%Y%m%d%H%M%S)
 IMAGE_TAG="$(basename $DOCKERFILE | cut -d . -f2-)-$TIMESTAMP"
 IMAGE_REPO=${2:-docker.io/olge404/molecule}
 
-podman build --file $DOCKERFILE --tag docker.io/olge404/molecule:$IMAGE_TAG .
-podman push $IMAGE_REPO:$IMAGE_TAG
+docker build --file $DOCKERFILE --tag docker.io/olge404/molecule:$IMAGE_TAG .
+docker push $IMAGE_REPO:$IMAGE_TAG
