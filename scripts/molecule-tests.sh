348d86bbc75f4afb0196959a85ce0f7ea9a5115d
OlGe404 <you@notpublic.com>
2024-12-18 20:01:46 +0100

    update molecule-tests.sh to perform tests for one role, if the role name is provided as $1

--- scripts/molecule-tests.sh
+++ scripts/molecule-tests.sh
@@ -1,13 +1,19 @@
 #!/bin/bash -e
 
 ROOT=$(git rev-parse --show-toplevel)
-ROLES=$(find $ROOT/roles -mindepth 1 -maxdepth 1 -type d)
+
+# Test all roles found in the "roles/" dir, if role is not provided as $1
+if [ -z "$1" ]; then
+    ROLES_TO_TEST=$(find "$ROOT/roles" -mindepth 1 -maxdepth 1 -type d)
+else
+    ROLES_TO_TEST="$ROOT/roles/$1"
+fi
 
 if [ -d "$ROOT/.venv" ]; then
     source $ROOT/.venv/bin/activate
 fi
 
-for role in $ROLES; do
+for role in $ROLES_TO_TEST; do
     cd $role
     if [ -d "molecule" ]; then
         printf "ℹ️  Starting molecule tests for '$(basename $role)' role \n\n" && molecule test;
