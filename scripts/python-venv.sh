cd71807c985f8d5ee9c02651dadbdea4f8251240
OlGe404 <you@notpublic.com>
2024-12-19 09:48:25 +0100

    add "pip install --upgrade pip setuptools wheel" to python-venv.sh script

--- scripts/python-venv.sh
+++ scripts/python-venv.sh
@@ -3,4 +3,5 @@
 ROOT=$(git rev-parse --show-toplevel)
 
 python3 -m venv $ROOT/.venv && source $ROOT/.venv/bin/activate
-pip install -r requirements.txt
+pip install --upgrade pip setuptools wheel
+pip install --upgrade -r requirements.txt
